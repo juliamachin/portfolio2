e0759826377a7225b3413a5aa5ce6490c907cec3
juliamachin <juliaamachin@gmail.com>
2022-11-02 17:25:16 -0700

    Set up components and imports.

---
+++ src/Components/Contact/Contact.js
@@ -0,0 +1,9 @@
+import "./Contact.css";
+
+const Contact = () => {
+  <div>
+    <h1>Contact Page</h1>
+  </div>;
+};
+
+export default Contact;
---
+++ src/Components/Portfolio/Portfolio.js
@@ -0,0 +1,9 @@
+import "./Portfolio.css";
+
+const Portfolio = () => {
+  <div>
+    <h1>Portfolio Page</h1>
+  </div>;
+};
+
+export default Portfolio;
