480e7b4f808d25b8d8d61c18239c6fcc15b37ea7
juliamachin <juliaamachin@gmail.com>
2022-11-15 19:44:50 -0800

    imported data and arrow icons into porfolio component

--- src/App.js
+++ src/App.js
@@ -4,18 +4,19 @@ import Home from "./Components/Home/Home";
 import About from "./Components/About/About";
 import Portfolio from "./Components/Portfolio/Portfolio";
 import Contact from "./Components/Contact/Contact";
-import Links from "./Components/Links/Links"
+import Links from "./Components/Links/Links";
+import Data from "./Components/Portfolio/Data";
 
 function App() {
   return (
     <div className="App">
       <Links />
-      
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/portfolio" element={<Portfolio slides={Data} />} />
       </Routes>
     </div>
   );
--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -1,15 +1,20 @@
 import "./Portfolio.css";
-import Data from './Data'
+import Data from "./Data";
+import { useState } from "react";
+import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from "react-icons/fa";
 
-const Portfolio = () => {
+const Portfolio = ({ slides }) => {
+  const [current, setCurrent] = useState(0);
+  const length = slides.length;
+
   return (
-    <div>
+    <section className='slider'>
+      <FaArrowAltCircleLeft className="left" />
+      <FaArrowAltCircleRight className="right" />
       {Data.map((slide, index) => {
-        return (
-        <img src={slide.image} alt="images" />
-        )
+        return <img src={slide.image} alt="images" />;
       })}
-    </div>
+    </section>
   );
 };
 
