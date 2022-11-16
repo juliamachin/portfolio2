0270b91d36311e898112b4275edca42fbbae4405
juliamachin <juliaamachin@gmail.com>
2022-11-16 15:13:39 -0800

    Created functions for next and previous slides

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -7,10 +7,24 @@ const Portfolio = ({ slides }) => {
   const [current, setCurrent] = useState(0);
   const length = slides.length;
 
+  const nextSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1)
+  }
+
+  const prevSlide = () => {
+    setCurrent(current === 0 ? length -1 : current - 1) 
+  }
+
+  console.log(current)
+
+  if (!Array.isArray(slides) || slides.length <= + 0) {
+    return null;
+  }
+
   return (
     <section className="slider">
-      <FaArrowAltCircleLeft className="left" />
-      <FaArrowAltCircleRight className="right" />
+      <FaArrowAltCircleLeft className="left" onClick={prevSlide} />
+      <FaArrowAltCircleRight className="right" onClick={nextSlide} />
       {Data.map((slide, index) => {
         return <img src={slide.image} alt="images" className="image" />;
       })}
