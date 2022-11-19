cecdfbe858f866ebc8d11da43c5e536de1dc402c
juliamachin <juliaamachin@gmail.com>
2022-11-18 18:01:55 -0800

    added deploy links to data file

--- src/Components/Portfolio/Data.js
+++ src/Components/Portfolio/Data.js
@@ -1,9 +1,11 @@
 const Data = [
     {
-        image: '/Images/SomBase.jpg'
+        image: '/Images/SomBase.jpg',
+        deploy: 'https://veggie-tales00.github.io/Project3-Client/'
     },
     {
-        image: '/Images/MadLibs.jpg'
+        image: '/Images/MadLibs.jpg',
+        deploy: 'https://juliamachin.github.io/madlibz-app/'
     }
 ]
 
