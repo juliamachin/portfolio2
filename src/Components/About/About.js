327c599be972e5c319b91c7b9d89cee8756765ee
juliamachin <juliaamachin@gmail.com>
2022-11-28 15:46:24 -0800

    Added title about paragraph and image tag

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -3,7 +3,9 @@ import "./About.css"
 const About = () => {
     return (
         <div>
-            <h1>About Page</h1>
+            <h2>About Me</h2>
+            <img src="" alt="portrait" />
+            <p>A full-stack software developer with a background in the performing arts and communications. Dedicated and passionate about creative problem solving which has encouraged an abstract mindset that cultivates engaging web applications with the user in mind.</p>
         </div>
     )
 }
