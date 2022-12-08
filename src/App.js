0305bbd1a66da6fcd1b5297595a075db6fc5c5bb
juliamachin <juliaamachin@gmail.com>
2022-12-07 17:11:20 -0800

    adjusted paths

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Layout />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/portfolio2" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/portfolio" element={<Portfolio slides={Data} />} />
