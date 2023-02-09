b9637f2af91aa5f7643e37d629a2736d7565cf8b
juliamachin <juliaamachin@gmail.com>
2023-02-09 11:53:37 -0800

    made tweaks to about section

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -13,10 +13,10 @@ const About = () => {
         alt="portrait"
       />
       <p>
-        A full-stack software developer with a background in the performing arts
-        and communications. Dedicated and passionate about creative problem
-        solving which has encouraged an abstract mindset that cultivates
-        engaging web applications with the user in mind.
+        A full-stack software developer with a background in performing arts and
+        communications. Dedicated and passionate about creative problem-solving
+        which has encouraged an abstract mindset that cultivates engaging web
+        applications with the user in mind.
       </p>
     </div>
   );
